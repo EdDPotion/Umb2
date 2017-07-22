d4a60adffbbda11df7c7916a0e354f5a44c7ae08
EdDPotion <ed.davison@digitalpotion.co.uk>
2017-07-22 15:59:17 +0100

    commit

---
+++ Umb2/Controllers/SiteLayoutController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web.Mvc;
+using System.Web.Mvc;
+
+
+namespace Umb2.Controllers
+{
+    public class SiteLayoutController : SurfaceController
+    {
+        public ActionResult RenderHeader()
+        {
+            return PartialView("~/Views/Partials/SiteLayout/_Header.cshtml");
+        }
+
+        public ActionResult RenderFooter()
+        {
+            return PartialView("~/Views/Partials/SiteLayout/_Footer.cshtml");
+        }
+    }
+}
